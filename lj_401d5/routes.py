ee3a321a621c53905692a0119440a1e31226096a
= <garnaat.ben@gmail.com>
2016-12-19 22:20:09 -0800

    map step1 routes and views

--- lj_401d5/routes.py
+++ lj_401d5/routes.py
@@ -1,4 +1,8 @@
 def includeme(config):
     """This function adds routes to configurator."""
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    # config.add_route('home', '/')
+    config.add_route('lj_index', '/')
+    config.add_route('lj_detail', '/lj/{id:\d+}')
+    config.add_route('lj_create', '/lj/new')
+    config.add_route('lj_update', '/lj/{id:\d+}/edit')
--- lj_401d5/views.py
+++ lj_401d5/views.py
@@ -11,11 +11,35 @@ import os
 HERE = os.path.dirname(__file__)
 
 
-def home_page(request):
-    imported_text = open(os.path.join(HERE, 'sample.txt')).read()
-    return Response(imported_text)
-    return Response("This is a home page.")
+# def home_page(request):
+#     imported_text = open(os.path.join(HERE, 'sample.txt')).read()
+#     return Response(imported_text)
+#     return Response("This is a home page.")
 
 
 def includeme(config):
-    config.add_view(home_page, route_name='home')
+    # config.add_view(home_page, route_name='home')
+    config.add_view(lj_index, route_name='lj_index')
+    config.add_view(lj_detail, route_name='lj_detail')
+    config.add_view(lj_create, route_name='lj_create')
+    config.add_view(lj_update, route_name='lj_update')
+
+
+def lj_index(request):
+    imported_text = open(os.path.join(HERE, 'lj_index.html')).read()
+    return Response(imported_text)
+
+
+def lj_detail(request):
+    imported_text = open(os.path.join(HERE, 'lj_detail.html')).read()
+    return Response(imported_text)
+
+
+def lj_create(request):
+    imported_text = open(os.path.join(HERE, 'lj_create.html')).read()
+    return Response(imported_text)
+
+
+def lj_update(request):
+    imported_text = open(os.path.join(HERE, 'lj_update.html')).read()
+    return Response(imported_text)
